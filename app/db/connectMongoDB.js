fdb04f8fbd1594d2b75b6d65143c55074b671e93
valentindj <valentindj@starway-media.com>
2024-02-01 19:33:15 +0200

    debug deploy

--- app/db/connectMongoDB.js
+++ app/db/connectMongoDB.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const MONGO_URI = process.env.MONGO_URI;
 async function connectToMongoDB(req, res, next) {
     try {
+        console.log(MONGO_URI)
         await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
         console.log('Connected to MongoDB');
         next();
